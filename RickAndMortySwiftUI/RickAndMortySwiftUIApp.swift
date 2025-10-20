f24a3f34b41af604fdd8d1af0a23495b8e105902
jeria93 <39989194+jeria93@users.noreply.github.com>
2025-10-20 13:27:13 +0200

    Initial Commit

---
+++ RickAndMortySwiftUI/RickAndMortySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortySwiftUIApp.swift
+//  RickAndMortySwiftUI
+//
+//  Created by Nicholas Samuelsson Jeria on 2025-10-20.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
